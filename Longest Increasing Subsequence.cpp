0af6fab418b82f522115e9deb7c247eebc032dbe
Himanshu Kapoor <80637998+Hk0509@users.noreply.github.com>
2023-07-16 18:19:21 +0530

    Create Longest Increasing Subsequence.cpp

---
+++ Longest Increasing Subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        for(int i=1; i<n; i++){
+            if(ans.back()<nums[i])
+            ans.push_back(nums[i]);
+            else{
+                int index = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+                ans[index] = nums[i]; 
+            }
+        }
+        return ans.size();
+    }
+};
