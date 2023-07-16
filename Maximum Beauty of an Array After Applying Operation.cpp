c6f3353a1933a34d96cf9939c61bb0ffe12bdab8
Himanshu Kapoor <80637998+Hk0509@users.noreply.github.com>
2023-07-16 23:28:12 +0530

    Create Maximum Beauty of an Array After Applying Operation.cpp

---
+++ Maximum Beauty of an Array After Applying Operation.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int start = 0, ans = 0;
+        for(int i=0; i<nums.size(); i++){
+            while(nums[i] - k > nums[start] + k){
+                start++;
+            }
+            ans = max(ans, i-start+1);
+        }
+        return ans;
+    }
+};
