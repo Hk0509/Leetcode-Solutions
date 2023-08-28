4ea4fc700e3628f95dfca8e46dfbd7dba583fa7e
Himanshu Kapoor <80637998+Hk0509@users.noreply.github.com>
2023-08-28 22:14:41 +0530

    Create Implement Stack using Queues.cpp

---
+++ Implement Stack using Queues.cpp
@@ -0,0 +1,37 @@
+class MyStack {
+public:
+    queue<int> q;
+    
+    void push(int x) {
+        q.push(x);
+        int size = q.size() - 1;
+        while(size--){
+            int tp = q.front();
+            q.pop();
+            q.push(tp);
+        }
+    }
+    
+    int pop() {
+        int ans = q.front();
+        q.pop();
+        return ans;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
