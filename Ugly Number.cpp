37f66632672d768335fe3979907b809c4ea5efa2
Himanshu Kapoor <80637998+Hk0509@users.noreply.github.com>
2023-08-31 22:12:10 +0530

    Create Ugly Number.cpp

---
+++ Ugly Number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<1) return 0;
+        while(n%5 == 0){
+            n = n/5;
+        }
+        while(n%3 == 0){
+            n = n/3;
+        }
+        while(n%2 == 0){
+            n = n/2;
+        }
+        if(n>1) return 0;
+        return 1; 
+    }
+};
