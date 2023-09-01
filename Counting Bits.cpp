cb5f324c8f077cdbc5f55d353584ff5d1317f656
Himanshu Kapoor <80637998+Hk0509@users.noreply.github.com>
2023-09-01 22:14:14 +0530

    Create Counting Bits.cpp

---
+++ Counting Bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i=0; i<=n; i++){
+            int cnt = __builtin_popcount(i);
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+};
