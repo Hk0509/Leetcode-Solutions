b23dc0a530d929a52f943042c5d40be2d3d53f5e
Himanshu Kapoor <80637998+Hk0509@users.noreply.github.com>
2023-12-03 23:47:18 +0530

    Create Find the Peaks.cpp

---
+++ Find the Peaks.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        vector<int> ans;
+        int n = mountain.size();
+        for(int i=1; i<n-1; i++){
+            if(mountain[i] > mountain[i+1] && mountain[i] > mountain[i-1]) ans.push_back(i);
+        }
+        return ans;
+    }
+};
