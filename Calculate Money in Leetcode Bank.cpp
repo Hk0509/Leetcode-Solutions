49c248a673f7fd171582d054dd0b8821070bb1b6
Himanshu Kapoor <80637998+Hk0509@users.noreply.github.com>
2023-12-06 23:47:09 +0530

    Update Calculate Money in Leetcode Bank.cpp

--- Calculate Money in Leetcode Bank.cpp
+++ Calculate Money in Leetcode Bank.cpp
@@ -1,15 +1,31 @@
+//O(1) Solution
 class Solution {
 public:
-    int totalMoney(int n) {
-        double ans = 0, curr = 1;
-        double rem = n%7;
-        n = n - n%7;
-        for(double i=1; i<=n; i+=7){
-            double weekSum = 3.5 * (2*curr + (6));
-            curr++;
-            ans += weekSum;
-        }
-        ans += rem/2 * (2*curr + (rem - 1));
+    int totalMoney(int n) { 
+        double rem = n%7; //Remaining Days
+        n = n - n%7; //Remove extra days to make complete weeks
+        double weeks = n/7; // Number of weeks
+        //Derived Sum of series for weeks
+        double ans = 7*(3*weeks + (weeks * (weeks + 1)/2));  
+        //Remaining Days
+        ans += rem/2 * (2*(weeks+1) + (rem - 1));
         return ans;
     }
 };
+
+// O(N) Solution
+// class Solution {
+// public:
+//     int totalMoney(int n) {
+//         double ans = 0, curr = 1;
+//         double rem = n%7;
+//         n = n - n%7;
+//         for(double i=1; i<=n; i+=7){
+//             double weekSum = 3.5 * (2*curr + (6));
+//             curr++;
+//             ans += weekSum;
+//         }
+//         ans += rem/2 * (2*curr + (rem - 1));
+//         return ans;
+//     }
+// };
