1991cc0874d563e590cfb49c42d3d2c5af2ebd22
Himanshu Kapoor <80637998+Hk0509@users.noreply.github.com>
2023-12-20 23:37:19 +0530

    Update Buy Two Chocolates.cpp

--- Buy Two Chocolates.cpp
+++ Buy Two Chocolates.cpp
@@ -1,12 +1,33 @@
+//O(n) Solution
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(), prices.end());
-        int leftover = money - (prices[0] + prices[1]);
-        return leftover < 0 ? money : leftover;
+        int mini = 1e9, mini2 = 1e9;
+        for(auto it: prices){
+            if(it < mini){
+                mini2 = mini;
+                mini = it;
+            }
+            else if(it < mini2){
+                mini2 = it;
+            }
+        }
+        int left = money - mini - mini2;
+        return left < 0 ? money : left;
     }
 };
 
+//O(logn) Solution
+// class Solution {
+// public:
+//     int buyChoco(vector<int>& prices, int money) {
+//         sort(prices.begin(), prices.end());
+//         int leftover = money - (prices[0] + prices[1]);
+//         return leftover < 0 ? money : leftover;
+//     }
+// };
+
+//O(logn) during Contest Solution
 // class Solution {
 // public:
 //     int buyChoco(vector<int>& prices, int money) {
