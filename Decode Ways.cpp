634fa802ed5c93821f42f48a4ced9a2e789e770f
Himanshu Kapoor <80637998+Hk0509@users.noreply.github.com>
2023-12-25 19:53:03 +0530

    Update Decode Ways.cpp

--- Decode Ways.cpp
+++ Decode Ways.cpp
@@ -4,18 +4,17 @@ public:
     int solve(int i, int n, string &s){
         if(i == n) return 1;
         if(i > n) return 0;
-        if(s[i] == '0' ||  (s[i] >= '3' && s[i+1] == '0')) return 0; 
+        if(s[i] == '0') return 0; 
 
         if(dp[i] != -1) return dp[i];
 
-        int take = 0, notTake = 0;
-        if(i != n-1 && (s[i] == '2' && s[i+1] <= '6') || (s[i] == '1') || s[i+1] == '0'){
-            take = solve(i+2, n, s);
+        int take2 = 0;
+        if((s[i] == '2' && s[i+1] <= '6') || (s[i] == '1')){
+            take2 = solve(i+2, n, s);
         }
-        if(s[i+1] != '0')
-        notTake = solve(i+1, n, s);
+        int take1 = solve(i+1, n, s);
 
-        return dp[i] = take + notTake;
+        return dp[i] = take2 + take1;
     }
     int numDecodings(string s) {
         int n = s.size();
