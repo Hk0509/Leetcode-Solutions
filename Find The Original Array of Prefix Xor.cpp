bdf15ac251650a69e2ffb3fc4095dc5333215936
Himanshu Kapoor <80637998+Hk0509@users.noreply.github.com>
2023-12-29 22:26:26 +0530

    Update Find The Original Array of Prefix Xor.cpp

--- Find The Original Array of Prefix Xor.cpp
+++ Find The Original Array of Prefix Xor.cpp
@@ -3,8 +3,7 @@ public:
     vector<int> findArray(vector<int>& pref) {
         vector<int> ans;
         ans.push_back(pref[0]);
-        for(int i=1; i<pref.size(); i++)
-        {
+        for(int i=1; i<pref.size(); i++){
             int x = pref[i-1] ^ pref[i];
             ans.push_back(x);
         }
