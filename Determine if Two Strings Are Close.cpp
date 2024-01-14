a13ceba9140336e02b93f3023da6a33e00470e0a
Himanshu Kapoor <80637998+Hk0509@users.noreply.github.com>
2024-01-14 22:31:07 +0530

    Create Determine if Two Strings Are Close.cpp

---
+++ Determine if Two Strings Are Close.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if(word1.size() != word2.size()) return 0;
+        sort(word1.begin(), word1.end());
+        sort(word2.begin(), word2.end());
+
+        if(word1 == word2) return 1;
+
+        map<char, int> mpp1, mpp2;
+        for(auto it: word1){
+            mpp1[it]++;
+        }
+        for(auto it: word2){
+            mpp2[it]++;
+        }
+        
+        map<int, int> mpp3;
+
+        for(auto it: mpp1){
+            if(mpp2[it.first] == 0) return 0;
+            mpp3[it.second]++;
+        }
+        for(auto it: mpp2){
+            if(mpp1[it.first] == 0) return 0;
+            mpp3[it.second]--;
+        }
+
+        for(auto it: mpp3){
+            if(it.second != 0) return 0;   
+        }
+        
+        return 1;
+    }
+};
