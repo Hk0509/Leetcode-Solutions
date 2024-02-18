b504b34fcf77a8b9a5b66e504668f40c4c892e9b
Himanshu Kapoor <80637998+Hk0509@users.noreply.github.com>
2024-02-18 18:22:00 +0530

    Update Climbing Stairs.cpp

--- Climbing Stairs.cpp
+++ Climbing Stairs.cpp
@@ -1,18 +1,37 @@
+// 18 Feb
 class Solution {
-private:
-    int solve(int n, vector<int>& dp){
-        if(n <= 0) return n+1;
-        
+public:
+    int dp[50];
+    int solve(int n){
+        if(n < 0) return 0;
+        if(n <= 0) return 1;
+
         if(dp[n] != -1) return dp[n];
 
-        int one = solve(n-1, dp);
-        int two = solve(n-2, dp);
-
-        return dp[n] = one + two;
+        return dp[n] = solve(n-1) + solve(n-2);
     }
-public:
-    int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        return solve(n, dp);
+    int climbStairs(int n){
+        memset(dp, -1, sizeof(dp));
+       return solve(n);
     }
 };
+
+
+// class Solution {
+// private:
+//     int solve(int n, vector<int>& dp){
+//         if(n <= 0) return n+1;
+        
+//         if(dp[n] != -1) return dp[n];
+
+//         int one = solve(n-1, dp);
+//         int two = solve(n-2, dp);
+
+//         return dp[n] = one + two;
+//     }
+// public:
+//     int climbStairs(int n) {
+//         vector<int> dp(n+1, -1);
+//         return solve(n, dp);
+//     }
+// };
